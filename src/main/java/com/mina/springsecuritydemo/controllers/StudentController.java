e30f7bd8e520d632733ad9d69832715902704159
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-08 13:32:13 +0200

    create student controller

---
+++ src/main/java/com/mina/springsecuritydemo/controllers/StudentController.java
@@ -0,0 +1,9 @@
+package com.mina.springsecuritydemo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+}
