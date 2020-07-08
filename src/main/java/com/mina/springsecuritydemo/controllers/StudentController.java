b65aaa5d8dd786550a5ee561347aead81f843a69
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-08 13:44:24 +0200

    create student model and get student  rest endpoint

--- src/main/java/com/mina/springsecuritydemo/controllers/StudentController.java
+++ src/main/java/com/mina/springsecuritydemo/controllers/StudentController.java
@@ -1,9 +1,29 @@
 package com.mina.springsecuritydemo.controllers;
 
+import com.mina.springsecuritydemo.models.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "JAMES BOND"),
+            new Student(2, "Maria Jones"),
+            new Student(2, "Mina Rezkalla")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student " + studentId + " Does not exists"));
+    }
 }
