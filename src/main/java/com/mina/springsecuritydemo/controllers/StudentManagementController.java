6242fffa80acd0a8a43359b6ca7232d9bc3ffba8
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-09 10:51:54 +0200

    add application user permission to http methods

---
+++ src/main/java/com/mina/springsecuritydemo/controllers/StudentManagementController.java
@@ -0,0 +1,45 @@
+package com.mina.springsecuritydemo.controllers;
+
+import com.mina.springsecuritydemo.models.Student;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "JAMES BOND"),
+            new Student(2, "Maria Jones"),
+            new Student(3, "Mina Rezkalla")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return STUDENTS;
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
+        System.out.println(studentId);
+    }
+}
--- src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.mina.springsecuritydemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.mina.springsecuritydemo.security.ApplicationUserPermission.*;
 import static com.mina.springsecuritydemo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -28,9 +30,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -50,9 +58,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails adminUser = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("admin"))
-                .roles(ADMIN.name()) // ROLE-STUDENT
+                .roles(ADMIN.name()) // ROLE-ADMIN
                 .build();
-        return new InMemoryUserDetailsManager(userDetails, adminUser);
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("admin"))
+                .roles(ADMINTRAINEE.name()) // ROLE-ADMINTRINEE
+                .build();
+        return new InMemoryUserDetailsManager(userDetails, adminUser, tomUser);
 
     }
 }
