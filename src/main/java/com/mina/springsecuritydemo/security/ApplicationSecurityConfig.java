167c7a84e53b951bf501d2dcba9719b02eae9a39
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-09 16:47:31 +0200

    remove comment out and unused imports

--- src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -3,7 +3,6 @@ package com.mina.springsecuritydemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,8 +13,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.mina.springsecuritydemo.security.ApplicationUserPermission.*;
-import static com.mina.springsecuritydemo.security.ApplicationUserRole.*;
+import static com.mina.springsecuritydemo.security.ApplicationUserRole.ADMIN;
+import static com.mina.springsecuritydemo.security.ApplicationUserRole.ADMINTRAINEE;
+import static com.mina.springsecuritydemo.security.ApplicationUserRole.STUDENT;
 
 @Configuration
 @EnableWebSecurity
@@ -37,10 +37,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                //.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
