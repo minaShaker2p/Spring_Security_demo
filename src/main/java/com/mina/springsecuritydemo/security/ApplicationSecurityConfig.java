1ed44553d0e32fc2cdea773ddaa13f4920620b9c
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-10 14:30:14 +0200

    add logout button in courses screen

--- src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -53,9 +54,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
                 .clearAuthentication(true)
                 .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID","remember-me")
+                .deleteCookies("JSESSIONID", "remember-me")
                 .logoutSuccessUrl("/login");
 
     }
