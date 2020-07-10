3a43a6406fc31eb04ee1bb27fb95442f5a01aee1
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-10 16:25:52 +0200

    Create Application user and Application user service

---
+++ src/main/java/com/mina/springsecuritydemo/auth/ApplicationUserService.java
@@ -0,0 +1,16 @@
+package com.mina.springsecuritydemo.auth;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationUserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
