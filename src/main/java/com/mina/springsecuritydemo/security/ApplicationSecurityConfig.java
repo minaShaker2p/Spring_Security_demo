a198bcb6d4f8ccb94b716549fead8d62cd343c4a
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-14 11:19:40 +0200

    add JWT token verifier filter

--- src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mina/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mina.springsecuritydemo.security;
 
 import com.mina.springsecuritydemo.auth.ApplicationUserService;
+import com.mina.springsecuritydemo.jwt.JwtTokenVerifier;
 import com.mina.springsecuritydemo.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -39,6 +40,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
